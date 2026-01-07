5788cf0c0e62db37ec4500f16a4037d065ba3a49
arihuz <ariicaza@espol.edu.ec>
2026-01-07 14:06:03 -0500

    Self Encapsulate Field
    crear getter y setter para que no entren directo de un solo a su datos privados

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,16 +1,20 @@
 public class Paciente extends Persona {
     private HistorialMedico historialMedico;
+
     public Paciente(String nombre, int edad, String genero, String direccion, String telefono, String correoElectronico) {
         super(nombre, edad, genero, direccion, telefono, correoElectronico);
-        this.historialMedico = new HistorialMedico();
+        this.setHistorialMedico(new HistorialMedico());
     }
 
     public HistorialMedico getHistorialMedico() {
         return historialMedico;
     }
 
+    public void setHistorialMedico(HistorialMedico historialMedico) {
+        this.historialMedico = historialMedico;
+    }
+
     public boolean esAdultoMayor(){
         return super.getEdad()>=65;
     }
-
 }
