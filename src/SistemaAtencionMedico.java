0a13f0dfe196f7bd61c10405725f1531b9671293
arihuz <ariicaza@espol.edu.ec>
2026-01-07 14:23:18 -0500

    error de falta util
    util

--- src/SistemaAtencionMedico.java
+++ src/SistemaAtencionMedico.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.function.Function;
 public class SistemaAtencionMedico {
     private List<Paciente> pacientes;
     private List<Medico> medicos;
